dade97cd2aa43d85f478db1d4a02b70238b8dfb0
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2022-01-23 13:54:39 +0200

    del

--- src/redux/contacts/contactsSelectors.jsx
+++
@@ -1,2 +0,0 @@
-export const getContactsFromState = (state) => state.contacts.entities;
-export const getFilterFromState = (state) => state.filter;
--- src/redux/filter/filter-reducer.jsx
+++
@@ -1,6 +0,0 @@
-import { filter } from "../filter/filter-actions";
-import { createReducer } from "@reduxjs/toolkit";
-
-export const filterReducer = createReducer("", {
-  [filter]: (state, action) => action.payload,
-});
